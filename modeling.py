45da8a80a336cc939df045beec3df851f56e0e4e
runtianw <144590276+runtianw@users.noreply.github.com>
2025-09-24 21:52:04 +0800

    Add files via upload

---
+++ modeling.py
@@ -0,0 +1,325 @@
+# coding=utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import copy
+import logging
+import math
+
+from os.path import join as pjoin
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from torch.nn import CrossEntropyLoss, Dropout, Softmax, Linear, Conv2d, LayerNorm
+from torch.nn.modules.utils import _pair
+from scipy import ndimage
+
+import models.configs as configs
+
+logger = logging.getLogger(__name__)
+
+ATTENTION_Q = "MultiHeadDotProductAttention_1/query"
+ATTENTION_K = "MultiHeadDotProductAttention_1/key"
+ATTENTION_V = "MultiHeadDotProductAttention_1/value"
+ATTENTION_OUT = "MultiHeadDotProductAttention_1/out"
+FC_0 = "MlpBlock_3/Dense_0"
+FC_1 = "MlpBlock_3/Dense_1"
+ATTENTION_NORM = "LayerNorm_0"
+MLP_NORM = "LayerNorm_2"
+
+def np2th(weights, conv=False):
+    """Possibly convert HWIO to OIHW."""
+    if conv:
+        weights = weights.transpose([3, 2, 0, 1])
+    return torch.from_numpy(weights)
+
+def swish(x):
+    return x * torch.sigmoid(x)
+
+ACT2FN = {"gelu": torch.nn.functional.gelu, "relu": torch.nn.functional.relu, "swish": swish}
+
+class LabelSmoothing(nn.Module):
+    """
+    NLL loss with label smoothing.
+    """
+    def __init__(self, smoothing=0.0):
+        """
+        Constructor for the LabelSmoothing module.
+        :param smoothing: label smoothing factor
+        """
+        super(LabelSmoothing, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+
+    def forward(self, x, target):
+        logprobs = torch.nn.functional.log_softmax(x, dim=-1)
+
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()
+
+class Attention(nn.Module):
+    def __init__(self, config):
+        super(Attention, self).__init__()
+        self.num_attention_heads = config.transformer["num_heads"]
+        self.attention_head_size = int(config.hidden_size / self.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = Linear(config.hidden_size, self.all_head_size)
+        self.key = Linear(config.hidden_size, self.all_head_size)
+        self.value = Linear(config.hidden_size, self.all_head_size)
+
+        self.out = Linear(config.hidden_size, config.hidden_size)
+        self.attn_dropout = Dropout(config.transformer["attention_dropout_rate"])
+        self.proj_dropout = Dropout(config.transformer["attention_dropout_rate"])
+
+        self.softmax = Softmax(dim=-1)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states):
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_probs = self.softmax(attention_scores)
+        weights = attention_probs
+        attention_probs = self.attn_dropout(attention_probs)
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+        attention_output = self.out(context_layer)
+        attention_output = self.proj_dropout(attention_output)
+        return attention_output, weights
+
+class Mlp(nn.Module):
+    def __init__(self, config):
+        super(Mlp, self).__init__()
+        self.fc1 = Linear(config.hidden_size, config.transformer["mlp_dim"])
+        self.fc2 = Linear(config.transformer["mlp_dim"], config.hidden_size)
+        self.act_fn = ACT2FN["gelu"]
+        self.dropout = Dropout(config.transformer["dropout_rate"])
+
+        self._init_weights()
+
+    def _init_weights(self):
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.normal_(self.fc1.bias, std=1e-6)
+        nn.init.normal_(self.fc2.bias, std=1e-6)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act_fn(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.dropout(x)
+        return x
+
+class Embeddings(nn.Module):
+    """Construct the embeddings from patch, position embeddings.
+    """
+    def __init__(self, config, img_size, in_channels=3):
+        super(Embeddings, self).__init__()
+        self.hybrid = None
+        img_size = _pair(img_size)
+
+        patch_size = _pair(config.patches["size"])
+        if config.split == 'non-overlap':
+            n_patches = (img_size[0] // patch_size[0]) * (img_size[1] // patch_size[1])
+            self.patch_embeddings = Conv2d(in_channels=in_channels,
+                                       out_channels=config.hidden_size,
+                                       kernel_size=patch_size,
+                                       stride=patch_size)
+        elif config.split == 'overlap':
+            n_patches = ((img_size[0] - patch_size[0]) // config.slide_step + 1) * ((img_size[1] - patch_size[1]) // config.slide_step + 1)
+            self.patch_embeddings = Conv2d(in_channels=in_channels,
+                                        out_channels=config.hidden_size,
+                                        kernel_size=patch_size,
+                                        stride=(config.slide_step, config.slide_step))
+        self.position_embeddings = nn.Parameter(torch.zeros(1, n_patches+1, config.hidden_size))
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size))
+
+        self.dropout = Dropout(config.transformer["dropout_rate"])
+
+    def forward(self, x):
+        B = x.shape[0]
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+
+        if self.hybrid:
+            x = self.hybrid_model(x)
+        x = self.patch_embeddings(x)
+        x = x.flatten(2)
+        x = x.transpose(-1, -2)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        embeddings = x + self.position_embeddings
+        embeddings = self.dropout(embeddings)
+        return embeddings
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super(Block, self).__init__()
+        self.hidden_size = config.hidden_size
+        self.attention_norm = LayerNorm(config.hidden_size, eps=1e-6)
+        self.ffn_norm = LayerNorm(config.hidden_size, eps=1e-6)
+        self.ffn = Mlp(config)
+        self.attn = Attention(config)
+
+    def forward(self, x):
+        h = x
+        x = self.attention_norm(x)
+        x, weights = self.attn(x)
+        x = x + h
+
+        h = x
+        x = self.ffn_norm(x)
+        x = self.ffn(x)
+        x = x + h
+        return x, weights
+
+class Part_Attention(nn.Module):
+    def __init__(self):
+        super(Part_Attention, self).__init__()
+
+    def forward(self, x):
+        length = len(x)
+        last_map = x[0]
+        for i in range(1, length):
+            last_map = torch.matmul(x[i], last_map)
+        last_map = last_map[:,:,0,1:]
+
+        _, max_inx = last_map.max(2)
+        return _, max_inx
+
+
+class Encoder(nn.Module):
+    def __init__(self, config):
+        super(Encoder, self).__init__()
+        self.layer = nn.ModuleList()
+        for _ in range(config.transformer["num_layers"] - 1):
+            layer = Block(config)
+            self.layer.append(copy.deepcopy(layer))
+        self.part_select = Part_Attention()
+        self.part_layer = Block(config)
+        self.part_norm = LayerNorm(config.hidden_size, eps=1e-6)
+
+    def forward(self, hidden_states):
+        attn_weights = []  # 保存所有层的注意力权重
+        for layer in self.layer:
+            # hidden_states, weights = layer(hidden_states)
+            # attn_weights.append(weights)  # 保存当前层的注意力权重
+            hidden_states, weights = layer(hidden_states, 28, 28)
+            attn_weights.append(weights)
+
+        # 使用 part_select 选择部分注意力
+        part_num, part_inx = self.part_select(attn_weights)
+        part_inx = part_inx + 1
+        parts = []
+        B, num = part_inx.shape
+        for i in range(B):
+            parts.append(hidden_states[i, part_inx[i, :]])
+        parts = torch.stack(parts).squeeze(1)
+
+        # 将选择的部分拼接到 cls token 前
+        concat = torch.cat((hidden_states[:, 0].unsqueeze(1), parts), dim=1)
+
+        # 通过 part_layer 进一步处理
+        part_states, part_weights = self.part_layer(concat)
+        attn_weights.append(part_weights)  # 将 part_layer 的注意力权重也加入 attn_weights
+        part_encoded = self.part_norm(part_states)
+
+        return part_encoded, attn_weights  # 返回编码结果和注意力权重
+
+
+class Transformer(nn.Module):
+    def __init__(self, config, img_size):
+        super(Transformer, self).__init__()
+        self.embeddings = Embeddings(config, img_size=img_size)
+        self.encoder = Encoder(config)
+
+    def forward(self, input_ids):
+        embedding_output = self.embeddings(input_ids)
+        encoded_layers, attention_weights = self.encoder(embedding_output)  # 移除 return_attention 参数
+        return encoded_layers, attention_weights
+
+
+class VisionTransformer(nn.Module):
+    def __init__(self, config, img_size=224, num_classes=21843, smoothing_value=0, zero_head=False):
+        super(VisionTransformer, self).__init__()
+        self.num_classes = num_classes
+        self.smoothing_value = smoothing_value
+        self.zero_head = zero_head
+        self.classifier = config.classifier
+        self.transformer = Transformer(config, img_size)
+        self.part_head = nn.Linear(config.hidden_size, num_classes)
+
+    def forward(self, x, labels=None, return_attention=False):
+        # 通过 Transformer 获取编码后的特征和注意力权重
+        encoded_layers, attn_weights = self.transformer(x)
+
+        if return_attention:
+            return encoded_layers, attn_weights  # 返回编码层和注意力权重
+
+        # 使用 [CLS] Token 的输出进行分类
+        part_logits = self.part_head(encoded_layers[:, 0])
+
+        if labels is not None:
+            if self.smoothing_value == 0:
+                loss_fct = nn.CrossEntropyLoss()
+            else:
+                loss_fct = LabelSmoothing(self.smoothing_value)
+            part_loss = loss_fct(part_logits.view(-1, self.num_classes), labels.view(-1))
+            contrast_loss = con_loss(encoded_layers[:, 0], labels.view(-1))
+            loss = part_loss + contrast_loss
+            return loss, part_logits
+        else:
+            return part_logits
+
+    def get_target_layer(self):
+        # 获取最后一层的 Attention 模块
+        return self.transformer.encoder.layer[-1].attn
+
+    def forward_features(self, x):
+        x = self.forward(x)[:-1]  # 或者根据实际情况截取
+        return x
+
+
+def con_loss(features, labels):
+    B, _ = features.shape
+    features = F.normalize(features)
+    cos_matrix = features.mm(features.t())
+    pos_label_matrix = torch.stack([labels == labels[i] for i in range(B)]).float()
+    neg_label_matrix = 1 - pos_label_matrix
+    pos_cos_matrix = 1 - cos_matrix
+    neg_cos_matrix = cos_matrix - 0.4
+    neg_cos_matrix[neg_cos_matrix < 0] = 0
+    loss = (pos_cos_matrix * pos_label_matrix).sum() + (neg_cos_matrix * neg_label_matrix).sum()
+    loss /= (B * B)
+    return loss
+
+CONFIGS = {
+    'ViT-B_16': configs.get_b16_config(),
+    'ViT-B_32': configs.get_b32_config(),
+    'ViT-L_16': configs.get_l16_config(),
+    'ViT-L_32': configs.get_l32_config(),
+    'ViT-H_14': configs.get_h14_config(),
+    'testing': configs.get_testing(),
+}
